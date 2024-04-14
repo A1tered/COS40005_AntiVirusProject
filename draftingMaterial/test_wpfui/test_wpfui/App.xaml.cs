a9d956ab7746bda4690baf2456746f45e11bee5c
Zach Smith <83221778+A1tered@users.noreply.github.com>
2024-04-14 16:21:37 +1000

    replace forms with wpf

---
+++ draftingMaterial/test_wpfui/test_wpfui/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace test_wpfui
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
