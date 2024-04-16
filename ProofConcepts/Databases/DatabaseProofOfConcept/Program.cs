e7d7f2c4eead20386f99d5f723f8c9cac47cc374
ChristopherTTT <103995646@student.swin.edu.au>
2024-04-16 16:31:06 +1000

    Database Proof of Concept
    Feel free to play with database, I made a intermediary database class with basic features that may break immediately, may show basic ways to do things.

---
+++ ProofConcepts/Databases/DatabaseProofOfConcept/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DatabaseProofOfConcept;
+
+Database databaseInstance = new Database();
+databaseInstance.CreateTable("exampleTable", ["primaryID", "textHolder"], ["INTEGER PRIMARY KEY", "TEXT"]);
+databaseInstance.InsertValuesExplicit("exampleTable", ["primaryID", "textHolder"], [$"{databaseInstance.IDCurrent}", "hi im text"]);
+string queryResult = databaseInstance.BasicQuery("exampleTable", ["*"]);
+Console.WriteLine(queryResult);
