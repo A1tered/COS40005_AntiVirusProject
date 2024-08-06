dc713463494dd7e01b9018ee65c7c0a59bcc2b65
pawan-gh <162069026+pawan-gh@users.noreply.github.com>
2024-08-06 11:21:02 +1000

    Add files via upload

---
+++ Template.cs
@@ -0,0 +1,26 @@
+﻿/**************************************************************************
+ * File:        [FileName].cs
+ * Author:      [Name]
+ * Description: [Brief description of what the file does]
+ * Last Modified: [Date]
+ * Libraries:   [Location Libraries / Dependencies]
+ **************************************************************************/
+
+using System; 
+// Add other necessary using directives here
+
+namespace [YourNamespace]
+{
+    class [YourClassName]
+    {
+        // Your class implementation goes here
+
+        static void Main(string[] args)
+{
+    // Your main method implementation goes here
+}
+}
+
+
+
+
