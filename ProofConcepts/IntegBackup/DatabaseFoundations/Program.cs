a7835174819d8b1f1e3879b16afbdfb1b4fc44b0
ChristopherTTT <103995646@student.swin.edu.au>
2024-08-20 14:57:04 +1000

    BackupFiles
    Going to start attempting to convert to Asynchronous, backing up files as a result

---
+++ ProofConcepts/IntegBackup/DatabaseFoundations/Program.cs
@@ -0,0 +1,26 @@
+﻿/**************************************************************************
+ * File:        [FileName].cs
+ * Author:      [Name]
+ * Description: [Brief description of what the file does]
+ * Last Modified: [Date]
+ * Libraries:   [Location Libraries / Dependencies]
+ **************************************************************************/
+
+using DatabaseFoundations;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IntegrityDatabaseIntermediary integrityDatabaseIntermediary = new("IntegrityDatabase", true);
+        integrityDatabaseIntermediary.Vacuum();
+/*        integrityDatabaseIntermediary.DeleteAll();
+        //integrityDatabaseIntermediary.AddEntry(@"C:\Users\yumcy\OneDrive\Desktop\UniversitySubjects\Archive");
+        Dictionary<string, string> dict = integrityDatabaseIntermediary.GetSetEntries(1, 100);
+        Console.WriteLine("Pair debug:");
+        foreach (KeyValuePair<string, string> pair in dict)
+        {
+            Console.WriteLine(pair);
+        }*/
+    }
+}
