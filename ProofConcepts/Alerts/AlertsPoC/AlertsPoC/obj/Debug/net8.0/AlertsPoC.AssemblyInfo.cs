5d14254b2cf7aa2976dc02d67fe667b944cb51d4
knownAnon <zacharyj456@gmail.com>
2024-08-25 15:14:45 +1000

    alerts basic functionality

--- ProofConcepts/Alerts/AlertsPoC/AlertsPoC/obj/Debug/net8.0/AlertsPoC.AssemblyInfo.cs
+++ ProofConcepts/Alerts/AlertsPoC/AlertsPoC/obj/Debug/net8.0/AlertsPoC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlertsPoC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91fd4daa9253e10adc3a4080648821e5fe8dbd13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b2bd73fb3fc5e66afeb3aadc8e08172e747e03d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlertsPoC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlertsPoC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
