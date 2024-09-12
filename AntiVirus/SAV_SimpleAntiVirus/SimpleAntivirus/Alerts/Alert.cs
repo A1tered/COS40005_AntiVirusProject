35a994ea192e7b6e9fe2bcac4cf34a74b0b58650
ChristopherTTT <103995646@student.swin.edu.au>
2024-09-12 15:34:43 +1000

    Alerts and Integrity Fixes
    + Alerts now aggregate within time frame
    + Freezing for INTEGRITY only fixed (Dirty fix)
    + Critical Bug fixed for Integrity resulting in it missing files completely.
    + Progress is now more responsive

--- AntiVirus/SAV_SimpleAntiVirus/SimpleAntivirus/Alerts/Alert.cs
+++ AntiVirus/SAV_SimpleAntiVirus/SimpleAntivirus/Alerts/Alert.cs
@@ -32,15 +32,26 @@ public class Alert
         Timestamp = DateTime.Now;
     }
 
-    public void DisplayAlert()
+    public void DisplayAlert(bool aggregateAlert = false, int aggregateAmount = 0)
     {
-        new ToastContentBuilder()
+        if (!aggregateAlert)
+        {
+            new ToastContentBuilder()
+                .AddCustomTimeStamp(Timestamp)
+                .AddText($"Alert: {Component} [Severity: {Severity}]")
+                .AddText(Message)
+                .AddText($"Suggested Action: {SuggestedAction}")
+                .Show();
+            System.Diagnostics.Debug.WriteLine($"[ALERT - {Timestamp}] [Component: {Component}] [Severity: {Severity}]\nMessage: {Message}\nSuggested Action: {SuggestedAction}\n");
+        }
+        else
+        {
+             new ToastContentBuilder()
             .AddCustomTimeStamp(Timestamp)
             .AddText($"Alert: {Component} [Severity: {Severity}]")
-            .AddText(Message)
-            .AddText($"Suggested Action: {SuggestedAction}")
+            .AddText($"Message: {Component} violations found in {aggregateAmount - 1}+ files")
+            .AddText("Suggested Action: Review protection history immediately!")
             .Show();
-
-        System.Diagnostics.Debug.WriteLine($"[ALERT - {Timestamp}] [Component: {Component}] [Severity: {Severity}]\nMessage: {Message}\nSuggested Action: {SuggestedAction}\n");
+        }
     }
 }
--- AntiVirus/SAV_SimpleAntiVirus/SimpleAntivirus/Alerts/AlertManager.cs
+++ AntiVirus/SAV_SimpleAntiVirus/SimpleAntivirus/Alerts/AlertManager.cs
@@ -2,14 +2,28 @@
 using System.Collections.Generic;
 using Microsoft.Data.Sqlite;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace SimpleAntivirus.Alerts;
 public class AlertManager
 {
     protected SqliteConnection _databaseConnection;
 
+    // Seconds (60 = 1 minute)
+    // How long to keep track of the amount of violations
+    private long _violationAmountTimeFrame;
+    private long _violationAmountTimeTracker;
+    // How long to supress violations.
+    private long _violationTimeFrame;
+    Dictionary<string, int> _violationIncidents;
+    Dictionary<string, long> _aggregateViolationTimeSent;
     public AlertManager()
     {
+        _violationAmountTimeFrame = 60;
+        _violationTimeFrame = 10;
+        _violationAmountTimeTracker = 0;
+        _violationIncidents = new();
+        _aggregateViolationTimeSent = new();
         string databasePath = "alerts.db"; // Specify the database path
         _databaseConnection = new SqliteConnection($"Data Source={databasePath}"); // Use Data Source for the SQLite connection string
         InitializeDatabase();
@@ -93,9 +107,43 @@ public class AlertManager
         return alerts;
     }
 
+    private int CompareSecoundTime(long time)
+    {
+        return Convert.ToInt32((Environment.TickCount64 - time) / 1000);
+    }
+
     public async Task LogAndDisplayAlertAsync(Alert alert)
     {
         await StoreAlertAsync(alert);
-        alert.DisplayAlert();
+        // Code to check for too many alerts at once.
+
+        // Restart time, for how long violations are kept.
+        if (CompareSecoundTime(_violationAmountTimeTracker) > _violationAmountTimeFrame)
+        {
+            _violationIncidents.Clear();
+            _violationAmountTimeFrame = Environment.TickCount64;
+        }
+
+        // Check if tracking amount of violations.
+        if (_violationIncidents.ContainsKey(alert.Component))
+        {
+            _violationIncidents[alert.Component] += 1;
+
+            // If within time frame, then we can send an alert, if not we must wait.
+            if (CompareSecoundTime(_aggregateViolationTimeSent[alert.Component]) > _violationTimeFrame)
+            {
+                // This is where we can indicate a warning for multiple alerts.
+                alert.DisplayAlert(true, _violationIncidents[alert.Component]);
+                _aggregateViolationTimeSent[alert.Component] = Environment.TickCount64;
+                // Erase values
+            }
+
+        }
+        else
+        {
+            _aggregateViolationTimeSent[alert.Component] = 0;
+            _violationIncidents[alert.Component] = 1;
+            alert.DisplayAlert();
+        }
     }
 }
