52643202a6b0380892125178dd7d24aba74b2e63
ChristopherTTT <103995646@student.swin.edu.au>
2024-09-19 13:08:31 +1000

    theme loaded (minor)
    + Loaded Event (VERY MINOR)

--- AntiVirus/SAV_SimpleAntiVirus/SimpleAntivirus/GUI/Views/Pages/DashboardPage.xaml.cs
+++ AntiVirus/SAV_SimpleAntiVirus/SimpleAntivirus/GUI/Views/Pages/DashboardPage.xaml.cs
@@ -52,6 +52,8 @@ namespace SimpleAntivirus.GUI.Views.Pages
         // On page loaded, determine the current theme and check if Dark mode is already enabled.
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
+
+            ThemeSwitch.IsChecked = true;
             if (ViewModel.CurrentTheme == Wpf.Ui.Appearance.ApplicationTheme.Dark)
             {
                 DarkModeChange(ThemeSwitch);
