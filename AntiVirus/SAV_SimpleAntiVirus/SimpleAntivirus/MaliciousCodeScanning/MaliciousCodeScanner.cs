3db7e5a2208074b7103bca717ac4095f87fa9022
joelparks112 <107848240+joelparks112@users.noreply.github.com>
2024-09-29 13:44:42 +1000

    Added visibility of current custom scan list in UI
    + On the ScannerPage, if you are selecting folders for a custom scan, you can now view the current list.
    
    [LIMITATIONS]
    - There is no current way to remove items from the list before beginning the scan, however the list is emptied after a successful or cancelled custom scan.

--- AntiVirus/SAV_SimpleAntiVirus/SimpleAntivirus/MaliciousCodeScanning/MaliciousCodeScanner.cs
+++ AntiVirus/SAV_SimpleAntiVirus/SimpleAntivirus/MaliciousCodeScanning/MaliciousCodeScanner.cs
@@ -46,6 +46,15 @@ namespace SimpleAntivirus.MaliciousCodeScanning
 
                 if (scanType == "quick")
                 {
+                    /* Directories chosen by doing a Google search on common quick scan locations
+                     * Most information regarding this topic is not public, for obvious security reasons, as antivirus companies do not wish for this information
+                     * to be available to attackers.
+                     * Common locations include: Scanning contents of active memory, program files, system files and startup items.
+                     * Memory scanning is out of scope for this project given the limited time constraints. 
+                     * Hence, Program Files, System files (The Windows directory) and the Startup directory are being scanned
+                     * A paper I found regarding this topic can be found here:
+                     * https://www.researchgate.net/profile/Oemer-Aslan-5/publication/321759536_Performance_Comparison_of_Static_Malware_Analysis_Tools_Versus_Antivirus_Scanners_To_Detect_Malware/links/5a30d86c0f7e9b0d50f905c3/Performance-Comparison-of-Static-Malware-Analysis-Tools-Versus-Antivirus-Scanners-To-Detect-Malware.pdf
+                    */
                     directories.AddRange
                     ([
                      $"C:\\Program Files",
