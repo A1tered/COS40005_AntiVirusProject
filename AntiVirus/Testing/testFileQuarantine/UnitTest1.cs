77133eb6acc5066dfacf55ee06b6d3758edec592
knownAnon <zacharyj456@gmail.com>
2024-09-30 21:32:40 +1000

    test project for alerts and file Quarantine

---
+++ AntiVirus/Testing/testFileQuarantine/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace testFileQuarantine
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
