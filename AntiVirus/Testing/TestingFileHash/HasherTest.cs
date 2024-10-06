f7415cbb1d490735aa1cfde9f7ce7dc34d653062
joelparks112 <107848240+joelparks112@users.noreply.github.com>
2024-10-06 21:12:36 +1100

    File hash scanning unit tests

--- AntiVirus/Testing/TestingFileHash/HasherTest.cs
+++ AntiVirus/Testing/TestingFileHash/HasherTest.cs
@@ -19,12 +19,14 @@ namespace TestingFileHash
         }
 
         [Test]
+        // Opens and hashes the file, returns hash of given file
         public void OpenHashFileTest()
         {
             Assert.That(_hasherStub.OpenHashFile(_directory), Is.EqualTo(_hash));
         }
 
         [Test]
+        // Hashes file, returns the hash of the filestream
         public void HashFileTest()
         {
             FileStream fileStreamTest = File.Open(_directory, FileMode.Open, FileAccess.Read, FileShare.ReadWrite | FileShare.Delete);
--- AntiVirus/Testing/TestingFileHash/obj/Debug/net8.0-windows10.0.17763.0/TestingFileHash.AssemblyInfo.cs
+++ AntiVirus/Testing/TestingFileHash/obj/Debug/net8.0-windows10.0.17763.0/TestingFileHash.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestingFileHash")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d09d11fa07cc22fd64168fccdfe7898806674fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d4ecc10d411b5633d90c465fb685453907cea0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestingFileHash")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestingFileHash")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
