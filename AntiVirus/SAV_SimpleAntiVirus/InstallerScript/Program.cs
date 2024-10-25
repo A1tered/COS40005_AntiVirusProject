df9c571998b1f448567893c82f87346908260880
pawan-gh <pawansingh2002.email@gmail.com>
2024-10-26 02:23:02 +1100

    Installer 1.2
    - Integrating ProgramData with Installer
    - Refining the Installer UI

---
+++ AntiVirus/SAV_SimpleAntiVirus/InstallerScript/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace InstallerScript
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\ProgramData\SimpleAntivirus";
+
+            try
+            {
+                if (Directory.Exists(path))
+                {
+                    Directory.Delete(path, true);
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
